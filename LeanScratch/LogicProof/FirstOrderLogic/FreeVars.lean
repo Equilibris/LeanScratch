b3212215db660cffb171ac21344188d3f2452a0a
William Sørensen <ws423@cam.ac.uk>
2024-12-30 15:24:22 +0000

    Misc free var work before we changed to Fin2s

--- LeanScratch/LogicProof/FirstOrderLogic/FreeVars.lean
+++ LeanScratch/LogicProof/FirstOrderLogic/FreeVars.lean
@@ -3,14 +3,17 @@ import LeanScratch.LogicProof.FirstOrderLogic.Formula
 
 namespace FOL
 
--- At this point in my implementation I had used a (V : Type) to represent
--- variables, this let variable names be expressed clearly and with
--- [DecidableEq V] boundness could also be shown. I have decided to
--- forgo this at present as it looks like we will be doing substitution,
--- substitution is just a royal pain without De Bujin indicies. For this
--- I have now swapped and will stay like this.
+-- The following paragraph is kept to document my thought process but is
+-- no longer applicable as we now use NPHOAS for encoding variables.
+-- 
+-- -- At this point in my implementation I had used a (V : Type) to represent
+-- -- variables, this let variable names be expressed clearly and with
+-- -- [DecidableEq V] boundness could also be shown. I have decided to
+-- -- forgo this at present as it looks like we will be doing substitution,
+-- -- substitution is just a royal pain without De Bujin indicies. For this
+-- -- I have now swapped and will stay like this.
 --
--- Free variables are encoded as out-of-range De Bujin indicies.
+-- -- Free variables are encoded as out-of-range De Bujin indicies.
 
 -- Desicion procedures for these are trivial
 inductive Term.Free (n : Nat) : Term TA → Prop
@@ -89,6 +92,117 @@ inductive Formula.Free : Nat → Formula TA PA → Prop
   | iffl  : Free n v → Free n (.iff v x)
   | iffr  : Free n v → Free n (.iff x v)
 
-  | exis : Free n.succ v → Free n (.exis v)
-  | univ : Free n.succ v → Free n (.univ v)
+  | exis : Free n (v $ .var n.succ) → Free n (.exis v)
+  | univ : Free n (v $ .var n.succ) → Free n (.univ v)
+
+def Term.fresh (n : Nat) : Term Arity → Nat
+  | .var v => max n v.succ
+  | .const _ args => mapper n args
+    where
+      mapper {l} (n : Nat) : Vec _ l → Nat
+        | hd %:: tl => mapper (hd.fresh n) tl
+        | %[] => n
+
+theorem Term.fresh.mono (term : Term Arity) (hle : n ≤ k) : n ≤ term.fresh k :=
+  match term with
+  | .var v => by simp only [fresh, Nat.succ_eq_add_one, le_max_iff, hle, true_or]
+  | .const _ args => by
+    simp [fresh]
+    exact mapper_walker hle args
+    where
+      mapper_walker {l} {n k : Nat} (hle : n ≤ k) : (x : Vec (Term Arity) l) → n ≤ mapper k x
+        | %[] => by simp only [mapper, hle]
+        | hd %:: tl => by
+          simp only [Nat.succ_eq_add_one, mapper]
+          have := Term.fresh.mono hd hle
+          exact mapper_walker this tl
+
+theorem Term.fresh.mono_strict (term : Term Arity) (hlt : n < k) : n < term.fresh k :=
+  match term with
+  | .var v => by simp only [fresh, Nat.succ_eq_add_one, lt_max_iff, hlt, true_or]
+  | .const _ args => by
+    simp [fresh]
+    exact mapper_walker hlt args
+    where
+      mapper_walker {l} {n k : Nat} (hlt : n < k) : (x : Vec (Term Arity) l) → n < mapper k x
+        | %[] => by simp only [mapper, hlt]
+        | hd %:: tl => by
+          simp only [Nat.succ_eq_add_one, mapper]
+          have := Term.fresh.mono hd hlt
+          exact mapper_walker this tl
+
+def Formula.fresh (n : Nat) : Formula TA PA → Nat
+  | .pred _ args => Term.fresh.mapper n args
+
+  | .neg v => v.fresh n
+
+  | .iff a b | .imp a b | .conj a b | .disj a b =>
+    b.fresh (a.fresh n)
+
+  | .univ v | .exis v =>
+    (v $ .var 0).fresh n
+
+theorem Formula.fresh.mono (term : Formula TA PA) (hle : n ≤ k) : n ≤ term.fresh k :=
+  match term with
+  | .pred _ args => by
+    simp [fresh]
+    exact Term.fresh.mono.mapper_walker hle args
+
+  | .neg v => by
+    simp [fresh]
+    exact mono v hle
+
+  | .iff a b | .imp a b | .conj a b | .disj a b => by
+    simp [fresh]
+    exact mono b $ mono a hle
+
+  | .univ v | .exis v => by
+    simp [fresh]
+    exact mono _ hle
+
+theorem Formula.fresh.mono_strict (term : Formula TA PA) (hle : n < k) : n < term.fresh k :=
+  match term with
+  | .pred _ args => by
+    simp [fresh]
+    exact Term.fresh.mono_strict.mapper_walker hle args
+
+  | .neg v => by
+    simp [fresh]
+    exact mono v hle
+
+  | .iff a b | .imp a b | .conj a b | .disj a b => by
+    simp [fresh]
+    exact mono b $ mono a hle
+
+  | .univ v | .exis v => by
+    simp [fresh]
+    exact mono _ hle
+
+theorem Formula.fresh_not_Free {form : Formula TA PA} (h : form.Free x) : x < form.fresh n :=
+  match form , h with
+  | .pred _ v, _ => sorry
+  | .neg v, .neg p =>
+    let p := fresh_not_Free (n := n) p
+    p
+
+  | .univ v, .univ x
+  | .exis v, .exis x => by
+    simp only [fresh]
+    -- does not hold
+    sorry
+
+  | .iff a b, .iffl v
+  | .imp a b, .impl v
+  | .conj a b, .conjl v
+  | .disj a b, .disjl v => by
+    simp only [fresh]
+    have := fresh_not_Free (n := n) v
+    exact Formula.fresh.mono_strict b this
+  | .iff a b, .iffr v
+  | .imp a b, .impr v
+  | .conj a b, .conjr v
+  | .disj a b, .disjr v => by
+    simp only [fresh]
+    exact fresh_not_Free v
+
 
