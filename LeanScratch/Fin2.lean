24191e7e58365c67d4ebf13bba59a49977938ee3
William Sørensen <ws423@cam.ac.uk>
2025-07-10 17:45:16 +0100

    first try at ordering Fin2

--- LeanScratch/Fin2.lean
+++ LeanScratch/Fin2.lean
@@ -5,14 +5,20 @@ import LeanScratch.Fintype2
 deriving instance Repr for Fin2
 deriving instance DecidableEq for Fin2
 
-instance Fin2.decEq : DecidableEq (Fin2 n) := fun
+def List.getFin2 : (l : List α) → Fin2 l.length → α
+  | hd :: _, .fz => hd
+  | _ :: tl, .fs x => tl.getFin2 x
+
+namespace Fin2
+
+instance decEq : DecidableEq (Fin2 n) := fun
   | .fz, .fs _ | .fs _, .fz => .isFalse Fin2.noConfusion
   | .fz, .fz => .isTrue rfl
   | .fs a, .fs b => match decEq a b with
     | .isTrue p => .isTrue $ p.rec rfl
     | .isFalse p => .isFalse (p ∘ (Fin2.fs.injEq _ _).mp)
 
-instance Fin2.instFintype2 : Fintype2 (Fin2 n) := match n with
+instance instFintype2 : Fintype2 (Fin2 n) := match n with
   | 0 => ⟨[], (fun x => by cases x), .nil⟩
   | n+1 =>
     let ⟨els, p, nodup⟩ := instFintype2 (n := n)
@@ -23,8 +29,40 @@ instance Fin2.instFintype2 : Fintype2 (Fin2 n) := match n with
 
 instance {v : Fin n} : Fin2.IsLT (v.val) n := ⟨v.isLt⟩
 
-def Fin2.ofFin (fin : Fin n) : Fin2 n := Fin2.ofNat' fin.val
+def ofFin (fin : Fin n) : Fin2 n := Fin2.ofNat' fin.val
 
-def List.getFin2 : (l : List α) → Fin2 l.length → α
-  | hd :: _, .fz => hd
-  | _ :: tl, .fs x => tl.getFin2 x
+inductive Le : {n : Nat} → Fin2 n → Fin2 n → Prop
+  | rfl : Le .fz .fz
+  | fz : Le .fz (.fs v)
+  | fs : Le a b → Le (.fs a) (.fs a)
+inductive Lt : {n : Nat} → Fin2 n → Fin2 n → Prop
+  | fz : Lt .fz (.fs v)
+  | fs : Lt a b → Lt (.fs a) (.fs a)
+
+instance : LE (Fin2 n) := ⟨Le⟩
+instance : LT (Fin2 n) := ⟨Lt⟩
+
+instance : Bot (Fin2 (n+1)) where
+  bot := .fz
+
+def bot_le : {n : Fin2 (n + 1)} → ⊥ ≤ n | .fz => .rfl | .fs _ => .fz
+
+instance : OrderBot (Fin2 (n+1)) where
+  bot_le _ := bot_le
+
+def LE.refl : ∀ (x : Fin2 n), x ≤ x
+  | .fz => .rfl
+  | .fs v => .fs $ refl v
+
+def LE.trans : {a b c : Fin2 n} → a ≤ b → b ≤ c → a ≤ c
+  | _,_,_, .rfl, x => x
+  | _,_,_, .fs ha, b => sorry
+  | _,_,_, .fz, .fs b => bot_le
+
+instance : PartialOrder (Fin2 n) where
+  lt := instLT_leanScratch.lt
+  lt_iff_le_not_le := sorry
+  le_refl := LE.refl
+  le_trans := sorry
+  le_antisymm := sorry
+
